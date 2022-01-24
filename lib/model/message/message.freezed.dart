cb56275dac19b754266418d3d5b232c14f2b2581
daisuke <grass536@gmail.com>
2022-01-24 17:38:31 +0900

    add message model

---
+++ lib/model/message/message.freezed.dart
@@ -0,0 +1,224 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'message.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Message _$MessageFromJson(Map<String, dynamic> json) {
+  return _Message.fromJson(json);
+}
+
+/// @nodoc
+class _$MessageTearOff {
+  const _$MessageTearOff();
+
+  _Message call(
+      {required String text,
+      required String postUserID,
+      @ServerTimestampConverter() required DateTime? createdAT}) {
+    return _Message(
+      text: text,
+      postUserID: postUserID,
+      createdAT: createdAT,
+    );
+  }
+
+  Message fromJson(Map<String, Object?> json) {
+    return Message.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $Message = _$MessageTearOff();
+
+/// @nodoc
+mixin _$Message {
+  String get text => throw _privateConstructorUsedError;
+  String get postUserID => throw _privateConstructorUsedError;
+  @ServerTimestampConverter()
+  DateTime? get createdAT => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $MessageCopyWith<Message> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MessageCopyWith<$Res> {
+  factory $MessageCopyWith(Message value, $Res Function(Message) then) =
+      _$MessageCopyWithImpl<$Res>;
+  $Res call(
+      {String text,
+      String postUserID,
+      @ServerTimestampConverter() DateTime? createdAT});
+}
+
+/// @nodoc
+class _$MessageCopyWithImpl<$Res> implements $MessageCopyWith<$Res> {
+  _$MessageCopyWithImpl(this._value, this._then);
+
+  final Message _value;
+  // ignore: unused_field
+  final $Res Function(Message) _then;
+
+  @override
+  $Res call({
+    Object? text = freezed,
+    Object? postUserID = freezed,
+    Object? createdAT = freezed,
+  }) {
+    return _then(_value.copyWith(
+      text: text == freezed
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String,
+      postUserID: postUserID == freezed
+          ? _value.postUserID
+          : postUserID // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAT: createdAT == freezed
+          ? _value.createdAT
+          : createdAT // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MessageCopyWith<$Res> implements $MessageCopyWith<$Res> {
+  factory _$MessageCopyWith(_Message value, $Res Function(_Message) then) =
+      __$MessageCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String text,
+      String postUserID,
+      @ServerTimestampConverter() DateTime? createdAT});
+}
+
+/// @nodoc
+class __$MessageCopyWithImpl<$Res> extends _$MessageCopyWithImpl<$Res>
+    implements _$MessageCopyWith<$Res> {
+  __$MessageCopyWithImpl(_Message _value, $Res Function(_Message) _then)
+      : super(_value, (v) => _then(v as _Message));
+
+  @override
+  _Message get _value => super._value as _Message;
+
+  @override
+  $Res call({
+    Object? text = freezed,
+    Object? postUserID = freezed,
+    Object? createdAT = freezed,
+  }) {
+    return _then(_Message(
+      text: text == freezed
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String,
+      postUserID: postUserID == freezed
+          ? _value.postUserID
+          : postUserID // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAT: createdAT == freezed
+          ? _value.createdAT
+          : createdAT // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Message extends _Message with DiagnosticableTreeMixin {
+  const _$_Message(
+      {required this.text,
+      required this.postUserID,
+      @ServerTimestampConverter() required this.createdAT})
+      : super._();
+
+  factory _$_Message.fromJson(Map<String, dynamic> json) =>
+      _$$_MessageFromJson(json);
+
+  @override
+  final String text;
+  @override
+  final String postUserID;
+  @override
+  @ServerTimestampConverter()
+  final DateTime? createdAT;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Message(text: $text, postUserID: $postUserID, createdAT: $createdAT)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Message'))
+      ..add(DiagnosticsProperty('text', text))
+      ..add(DiagnosticsProperty('postUserID', postUserID))
+      ..add(DiagnosticsProperty('createdAT', createdAT));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _Message &&
+            const DeepCollectionEquality().equals(other.text, text) &&
+            const DeepCollectionEquality()
+                .equals(other.postUserID, postUserID) &&
+            const DeepCollectionEquality().equals(other.createdAT, createdAT));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(text),
+      const DeepCollectionEquality().hash(postUserID),
+      const DeepCollectionEquality().hash(createdAT));
+
+  @JsonKey(ignore: true)
+  @override
+  _$MessageCopyWith<_Message> get copyWith =>
+      __$MessageCopyWithImpl<_Message>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_MessageToJson(this);
+  }
+}
+
+abstract class _Message extends Message {
+  const factory _Message(
+      {required String text,
+      required String postUserID,
+      @ServerTimestampConverter() required DateTime? createdAT}) = _$_Message;
+  const _Message._() : super._();
+
+  factory _Message.fromJson(Map<String, dynamic> json) = _$_Message.fromJson;
+
+  @override
+  String get text;
+  @override
+  String get postUserID;
+  @override
+  @ServerTimestampConverter()
+  DateTime? get createdAT;
+  @override
+  @JsonKey(ignore: true)
+  _$MessageCopyWith<_Message> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/model/message/message.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'message.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Message _$$_MessageFromJson(Map<String, dynamic> json) => _$_Message(
+      text: json['text'] as String,
+      postUserID: json['postUserID'] as String,
+      createdAT: const ServerTimestampConverter()
+          .fromJson(json['createdAT'] as Timestamp?),
+    );
+
+Map<String, dynamic> _$$_MessageToJson(_$_Message instance) =>
+    <String, dynamic>{
+      'text': instance.text,
+      'postUserID': instance.postUserID,
+      'createdAT': const ServerTimestampConverter().toJson(instance.createdAT),
+    };
