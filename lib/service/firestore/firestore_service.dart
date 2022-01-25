97329e4cb10a08651f0498d89871874564ae952d
daisuke <grass536@gmail.com>
2022-01-25 12:11:53 +0900

    add Auth側でUserNameが変更されたらFirestoreも更新するようにした

--- lib/service/firestore/firestore_service.dart
+++ lib/service/firestore/firestore_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_template_app/service/auth/firebase_auth_service.dart';
+import 'package:firebase_template_app/model/fire_user/fire_user.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class FirebaseFirestoreBase {
@@ -13,33 +14,38 @@ abstract class FirebaseFirestoreBase {
 class FirestoreService extends FirebaseFirestoreBase {
   FirestoreService(this._read);
 
+  // ignore: unused_field
   final Reader _read;
 
-  Query<Map<String, dynamic>> fetchProfile(String uid) {
-    final usersQuery = FirebaseFirestore.instance
-        .collection('users')
-        .where('uid', isEqualTo: uid)
-        .limit(1);
-    return usersQuery;
+  Query<Map<String, dynamic>> fetchProfile(String uid) =>
+      FirebaseFirestore.instance
+          .collection('users')
+          .where('uid', isEqualTo: uid)
+          .limit(1);
+
+  Query<Map<String, dynamic>> fetchRoom() =>
+      FirebaseFirestore.instance.collection('rooms').where('entrant');
+
+  Future<void> userDirectryUpdate(User? user) async {
+    if (user != null) {
+      final _path = _firestore.collection('users').doc(user.uid);
+
+      final _userDoc = await _path.get();
+      await _userDocProvider(_userDoc.exists, user, _path);
+    } else {
+      if (kDebugMode) {
+        print('user null');
+      }
+    }
   }
 
-  Query<Map<String, dynamic>> fetchRoom(String uid) {
-    final usersQuery =
-        FirebaseFirestore.instance.collection('rooms').where('entrant');
-    return usersQuery;
-  }
-
-  @override
-  Future<void> signUp(User user) async {
-    final _auth = FirebaseAuthService(_read);
-    final _path = _firestore.collection('users').doc(_auth.firebaseUID);
-
-    final _hasData = await _path.get();
-    //なかったら作る
-    if (!_hasData.exists) {
-      final providerData = user.providerData.isEmpty
-          ? 'anonymous'
-          : user.providerData[0].providerId;
+  Future<void> _userDocProvider(bool hasData, User user,
+      DocumentReference<Map<String, dynamic>> _path) async {
+    final providerData = user.providerData.isEmpty
+        ? 'anonymous'
+        : user.providerData[0].providerId;
+    //なかった時
+    if (!hasData) {
       await _path.set({
         'name': user.displayName ?? '',
         'uid': user.uid,
@@ -47,6 +53,26 @@ class FirestoreService extends FirebaseFirestoreBase {
         'provider': providerData,
         'createdAt': serverTimeStamp,
       });
+      //あった時
+    } else {
+      await _path.update({
+        'name': user.displayName ?? '',
+        // 'uid': user.uid,
+        'iconURL': user.photoURL ?? '',
+        'provider': providerData,
+        'updatedAt': serverTimeStamp,
+      });
+    }
+  }
+
+  @override
+  Future<void> signUp(User? user) async {
+    final _path = _firestore.collection('users').doc(user?.uid);
+
+    final _hasData = await _path.get();
+    //なかったら作る
+    if (!_hasData.exists) {
+      await userDirectryUpdate(user);
     }
   }
 }
--- lib/view/ui/navigator/navigator_model.dart
+++ lib/view/ui/navigator/navigator_model.dart
@@ -1,12 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_template_app/service/auth/firebase_auth_service.dart';
+import 'package:firebase_template_app/service/firestore/firestore_service.dart';
 import 'package:firebase_template_app/view/ui/flutter_fire/fire_profile_screen.dart';
 import 'package:firebase_template_app/view/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final bottomIndex = StateProvider<int>((ref) => 0);
-final navigatorProvider = ChangeNotifierProvider((ref) => NavigatorModel());
+final navigatorProvider =
+    ChangeNotifierProvider((ref) => NavigatorModel(ref.read));
 
 class NavigatorModel extends ChangeNotifier {
+  NavigatorModel(this._read);
+  final Reader _read;
+
+  void userNameInspecter(WidgetRef ref, BuildContext context) {
+    final _firestore = FirestoreService(_read);
+    ref.listen<User?>(
+      firebaseAuthProvider,
+      (oldValue, newValue) {
+        if (oldValue?.displayName == newValue?.displayName) {
+          return;
+        } else {
+          _firestore.userDirectryUpdate(newValue);
+        }
+      },
+      onError: (error, stackTrace) => debugPrint('$error'),
+    );
+  }
+
   final pageWidgets = [
     const HomeScreen(),
     const FireProfileScreen(),
