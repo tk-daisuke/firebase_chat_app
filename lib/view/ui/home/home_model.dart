9ee810eda17d59ee365b48c526431f0c2997adc8
daisuke <grass536@gmail.com>
2022-01-29 00:47:21 +0900

    add qr code 関係のパッケージを追加

--- lib/view/ui/home/home_model.dart
+++ lib/view/ui/home/home_model.dart
@@ -5,7 +5,6 @@ import 'package:firebase_template_app/service/auth/firebase_auth_service.dart';
 import 'package:firebase_template_app/service/firestore/firestore_service.dart';
 import 'package:firebase_template_app/service/url_service.dart';
 import 'package:firebase_template_app/service/dialog_service.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
