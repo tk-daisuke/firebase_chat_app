c019e68e432bcf6cd983d7097bd50202fa6a882a
daisuke <grass536@gmail.com>
2022-01-28 12:55:08 +0900

    update 統合

--- lib/service/url_service.dart
+++ lib/service/url_service.dart
@@ -1,7 +1,14 @@
+import 'package:url_launcher/url_launcher.dart';
+
 class UrlService {
   bool urlChecker(String? url) {
     final urlNullCheck = url != null;
     final isUrl = urlNullCheck ? Uri.parse(url).isAbsolute : false;
     return isUrl;
+  }
+
+  void launchURL(String _url) async {
+    final Error error = ArgumentError('開けませんでした$_url');
+    await canLaunch(_url) ? await launch(_url) : throw error;
   }
 }
